ab4f665adea4993877101b0537c3970a4ca0c70c
Shawn Salat <31082405+ThinkSalat@users.noreply.github.com>
2024-07-24 21:06:59 -0700

    create background.js that clears sidebery snapshots from history.

---
+++ background.js
@@ -0,0 +1,13 @@
+const removeSideberySnapshotsFromHistory = async () => {
+
+  let downloads = await browser.downloads.search({ query: ['snapshot'] });
+  const sideberySnapshots = downloads.filter(dl => dl.byExtensionName = 'Sidebery')
+
+  sideberySnapshots.forEach(snapshot => {
+    browser.downloads.erase({id: snapshot.id});
+  })
+}
+
+removeSideberySnapshotsFromHistory()
+
+setInterval(removeSideberySnapshotsFromHistory, 5 *60 * 1000)
