3290017a96a5c714818b4d7169d2c468477c35cf
DumbIdiot <erik_ego@hotmail.com>
2024-12-18 08:34:54 +0100

    changed to clear 5 seconds after download finishes

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
 const removeSideberySnapshotsFromHistory = async () => {
 
   let downloads = await browser.downloads.search({ query: ['snapshot'] });
-  const sideberySnapshots = downloads.filter(dl => dl.byExtensionName = 'Sidebery')
+  const sideberySnapshots = downloads.filter(dl => dl.byExtensionName === 'Sidebery');
 
   sideberySnapshots.forEach(snapshot => {
     browser.downloads.erase({id: snapshot.id});
-  })
-}
+  });
+};
 
-removeSideberySnapshotsFromHistory()
-
-setInterval(removeSideberySnapshotsFromHistory, 60 * 1000)
+browser.downloads.onChanged.addListener((downloadItem) => {
+  if (downloadItem.state && downloadItem.state.current === 'complete') {
+    setTimeout(removeSideberySnapshotsFromHistory, 5000);
+  }
+});
